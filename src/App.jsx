74c74bc83e67195718b01d6544ba6b857e3692a0
Rusty Fausak <rustyfausak@gmail.com>
2025-10-07 01:24:16 -0500

    splash index; learning tailwind; svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+import bgDance from './assets/bg-friendly.jpg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="antialiased min-h-dvh bg-cover bg-center" style={{ backgroundImage: `url(${bgDance})` }}>
+        <div className="backdrop-grayscale-25 backdrop-brightness-50 min-h-dvh p-5 flex flex-col justify-end">
+          <Header />
+          <main className="font-serif text-shadow-lg">
+            <h1 className="text-5xl md:text-6xl max-w-md mb-5">Together, We Heal Our Brains</h1>
+            <div className="text-xl mb-5 max-w-4xl">We are a peer-driven community for brain injury survivors and their loved ones. We provide connection, encouragement, and shared strength to help every member move forward together. </div>
+            <div className="py-5">
+              <a href="#" className="rounded bg-sky-600 p-4 text-xl text-white hover:bg-sky-500">
+                Find Your Peer &rarr;
+              </a>
+            </div>
+          </main>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>section</section>
+      <footer>
+        footer
+      </footer>
     </>
   )
 }
-
-export default App
